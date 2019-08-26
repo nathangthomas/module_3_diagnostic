6e58f8d571ef323c25717586065da342bbfae9fd
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-08-26 09:32:19 -0600

    Add test for search function

--- spec/features/testing_testing_one_two_three_spec.rb
+++
@@ -1,3 +0,0 @@
-#this is content to test that 'git push -u upstream master' worked
-#I also ran 'bundle exec figaro install' to set up my config/application/yml file
-#and added a directory for facades and services
